296e68e526dd470702b8431aa96e6905c34c107d
Michael James Williams <contactmichael@michaeljameswilliams.com>
2014-03-22 14:37:31 +0000

    Initial files from Damir Veapi

---
+++ src/Assets/Scripts/ShipMovementBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovementBehaviour : MonoBehaviour
+{
+	public float moveSpeed = 10f;
+	public float rotationSpeed = 180f;
+	
+	// Update is called once per frame
+	void Update()
+	{
+		var horizontalAxis = Input.GetAxis("Horizontal");
+		var verticalAxis = Input.GetAxis("Vertical");
+		
+		// Turn left or right on left/right buttons
+		transform.Rotate(0, 0, -horizontalAxis * Time.deltaTime * rotationSpeed);
+		
+		// Move forward
+		transform.Translate(Vector3.right * verticalAxis * Time.deltaTime * moveSpeed);
+	}
+}
